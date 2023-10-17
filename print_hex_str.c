aa26e08c148980c5c134c3be654bc63b8a44628a
EnimaOs <ing.devov@gmail.com>
2023-10-18 00:24:48 +0100

    testing print_hex_str

--- print_hex_str.c
+++ print_hex_str.c
@@ -1,39 +1,73 @@
 #include "main.h"
+#include <string.h>
+#include <unistd.h>
+/**
+ * helper_XS - a function that helps
+ * @bcount: count
+ * @num: num
+ * Return:nothing
+*/
+char *helper_XS(int bcount, unsigned int num)
+{
+	int reminder, i;
+	char binary, *s;
+
+	s  = malloc(sizeof(char) * bcount + 1);
+	if (s == NULL)
+		return (s);
+	for (i = bcount - 1; i != -1; i--)
+	{
+		reminder = num % 16;
+
+		if (reminder > 9)
+			binary = (reminder - 10) + 'A';
+		else
+			binary = reminder + '0';
+		s[i] = binary;
+		num = num / 16;
+	}
+
+	s[bcount] = '\0';
+
+	return (s);
+}
 
 /**
- * print_hex_str - ...
- * @ap:va_list
- * Return:length
- */
-int print_hex_str(va_list ap)
+ * print_hex_str - a function that prints the string.
+ * @args:variadic arguments
+ * Return:the number of characters printed
+*/
+int print_hex_str(va_list args)
 {
-	char *str;
-	int i, len = 0;
-	int check;
+	char *s, *str;
+	int len, i;
 
-	str = va_arg(ap, char *);
+	str = va_arg(args, char*);
+	len = 0;
 	if (str == NULL)
-		str = "(null)";
-	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i] < 32 || str[i] >= 127)
+		str = "(null)";
+		len = strlen(str);
+		write(STDOUT_FILENO, str, len);
+		return (len);
+	}
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if ((str[i] >= 0 && str[i] < 32) || (str[i] >= 127))
 		{
-			_putchar('\\');
-			_putchar('x');
-			len += 2;
-			check = str[i];
-			if (check < 16)
-			{
-				_putchar('0');
-				len++;
-			}
-			print_hexadecimal(check, &len);
+			s = helper_XS((2), str[i]);
+			len +=  write(STDOUT_FILENO, "\\x", 2);
+			len +=  write(STDOUT_FILENO, s, 2);
+			free(s);
 		}
 		else
 		{
-			_putchar(str[i]);
-			len++;
+			write(STDOUT_FILENO, &str[i], 1);
+			len = len + 1;
 		}
+		i++;
 	}
+
 	return (len);
 }
