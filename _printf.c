9b870ae7583dedca586df6cca8b41b05782a90b1
mai <maishalan00000@gmail.com>
2023-10-19 05:03:49 +0300

    uploading _printf file

--- _printf.c
+++ _printf.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * _printf - is a function that selects the correct function to print.
- * @format: identifier to look for.
- * Return: the length of the string.
- */
+* _printf - selects the correct function to print.
+* @format: identifier to look for.
+* Return: the length of the string.
+*/
 int _printf(const char * const format, ...)
 {
 	convert_match m[] = {
